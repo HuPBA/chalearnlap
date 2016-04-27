aaf556ddffa83b5ff4b5d35cb6ec61f19102539e
oriolUB <oriolsuils@gmail.com>
2016-04-27 19:02:43 +0200

    Partners biography and profile picture added

--- src/chalearnweb/urls.py
+++ src/chalearnweb/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -29,9 +28,6 @@ urlpatterns = [
 
 handler404 = 'views.handler404'
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG == True:
-    urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
